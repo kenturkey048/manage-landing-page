e30c7f4425185dbed58ed8db75cbe650de2ffc6a
kenturkey048 <kenturkey048@gmail.com>
2024-05-31 23:37:10 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        DarkBlue: 'hsl(12, 39%, 23%)',
+        DarkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(13, 100%, 96%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      }
+    },
+  },
+  plugins: [],
+}
+
